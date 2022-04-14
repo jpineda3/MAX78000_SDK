31280202dc4525bf4ad9f6e10649978ed248aee0
Lorne Smith <65424047+lorne-maxim@users.noreply.github.com>
2022-04-14 13:38:48 -0500

    Update tft_fthr.h

--- Libraries/Boards/MAX78000/Include/tft_fthr.h
+++ Libraries/Boards/MAX78000/Include/tft_fthr.h
@@ -1,2 +1,2 @@
-#warning tft_fthr.h has been deprecated.  Instead, use the appropriate dirver from Libraries/MiscDrivers/Display.
-#include tft_ili9341.h
+#warning tft_fthr.h has been deprecated.  Instead, use the appropriate driver from Libraries/MiscDrivers/Display.
+#include "tft_ili9341.h"
